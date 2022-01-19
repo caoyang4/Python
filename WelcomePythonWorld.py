c967aaf9c01346ea910f5c745ddeebd7c32a2403
caoyang42 <caoyang42@meituan.com>
2022-01-19 19:56:40 +0800

    python入坑之路

--- WelcomePythonWorld.py
+++ WelcomePythonWorld.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
-    print(f'{name}, welcome to Python World')  # 按 ⌘F8 切换断点。
+    print(f'Hi. {name}, welcome to python world!')  # 按 ⌘F8 切换断点。
 
 
 # 按间距中的绿色按钮以运行脚本。
