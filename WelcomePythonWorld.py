4a8cc36c95c11c815b8ce8a1e4b1c3fda7d554ed
caoyang42 <caoyang42@meituan.com>
2022-01-19 19:54:43 +0800

    python入坑之路

--- WelcomePythonWorld.py
+++ WelcomePythonWorld.py
@@ -6,11 +6,11 @@
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
+    print(f'{name}, welcome to Python World')  # 按 ⌘F8 切换断点。
 
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Young')
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
